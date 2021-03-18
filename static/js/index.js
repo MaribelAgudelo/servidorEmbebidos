354b31c4f3a05476dade93effc9d645f2acc74fb
MaribelAgudelo <80277071+MaribelAgudelo@users.noreply.github.com>
2021-03-18 10:38:46 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -41,7 +41,7 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("maribel.agudelo@unach.edu.ec//test");
+    client.subscribe("maribel.agudelo@unach.edu.ec/test");
     message = new Paho.MQTT.Message("hola desde la web");
     message.destinationName = "maribel.agudelo@unach.edu.ec/test1";
     client.send(message);
